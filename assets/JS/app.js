17fb93222a5e83bac39effe38c8f82f9fee93b4f
Bruno Gonçalves Ferreira <brungoncalvesferreira@outlook.com>
2023-08-17 23:51:08 -0300

    :boom: ajustando o mobile

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,42 +1,33 @@
-
 $(document).ready(function () {
-    site.eventos.init();
+  site.eventos.init()
 })
 
-var site = {};
+var site = {}
 
 site.eventos = {
-
-    init: () => {
-        site.metodos.carregarBotaoLigar();
-        site.metodos.carregarBotaoWhatsapp();
-    }
-
+  init: () => {
+    site.metodos.carregarBotaoLigar()
+    site.metodos.carregarBotaoWhatsapp()
+  },
 }
 
-var CELULAR_EMPRESA = '553230612136'; // Altere o telefone da Empresa aqui
+var CELULAR_EMPRESA = "553230612136" // Altere o telefone da Empresa aqui
 
 site.metodos = {
-    // Carrega o botão de ligar (telefone)
-    carregarBotaoLigar: () => {
+  // Carrega o botão de ligar (telefone)
+  carregarBotaoLigar: () => {
+    $("#btnLigar").attr("href", `tel:${CELULAR_EMPRESA}`)
+    $("#btnLigar2").attr("href", `tel:${CELULAR_EMPRESA}`)
+    $("#btnLigar3").attr("href", `tel:${CELULAR_EMPRESA}`)
+  },
 
-        $("#btnLigar").attr('href', `tel:${CELULAR_EMPRESA}`);
-        $("#btnLigar2").attr('href', `tel:${CELULAR_EMPRESA}`);
-        $("#btnLigar3").attr('href', `tel:${CELULAR_EMPRESA}`);
+  // Carrega o botão do WhatsApp
+  carregarBotaoWhatsapp: () => {
+    // Converte a URL
+    let encode = encodeURI("Olá! Gostaria de informações.")
+    let URL = `https://wa.me/${CELULAR_EMPRESA}?text=${encode}`
 
-
-    },
-
-    // Carrega o botão do WhatsApp
-    carregarBotaoWhatsapp: () => {
-
-        // Converte a URL
-        let encode = encodeURI("Olá! Gostaria de informações.");
-        let URL = `https://wa.me/${CELULAR_EMPRESA}?text=${encode}`;
-
-        $("#btnWhatsapp").attr('href', URL);
-        $("#btnWhatsapp2").attr('href', URL);
-
-    },
-
+    $("#btnWhatsapp").attr("href", URL)
+    $("#btnWhatsapp2").attr("href", URL)
+  },
 }
